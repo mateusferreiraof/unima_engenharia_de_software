ba90659a1ab2e3a0c577b02524e837289b79bd72
Mateus Ferreira <mateutrox@gmail.com>
2025-05-19 17:40:16 -0300

    Removido a variável conexao do app.py

--- app.py
+++ app.py
@@ -2,21 +2,6 @@ from PROJETO import server
 from PROJETO.database.conectar import ConectarBanco
 from PROJETO.database.criartabelas import CriarTabelas
 from PROJETO.database.criarbanco import CriarBanco
-import os
-from dotenv import load_dotenv
-import pymysql
-
-
-load_dotenv()  # carrega variáveis do .env
-
-conexao = pymysql.connect(
-    host=os.getenv('DB_HOST'),
-    user=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASSWORD'),
-    database=os.getenv('DB_NAME'),
-    port=int(os.getenv('DB_PORT', 3306))
-)
-
 
 bancodedados = ConectarBanco()
 conexao = bancodedados.conectar()
